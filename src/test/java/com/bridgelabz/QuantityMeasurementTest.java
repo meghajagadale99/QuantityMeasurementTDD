fa5e7bbacd03625a46671b7cb169cc8cd1945598
Megha <meghajagadale99@gmail.com>
2021-09-21 15:25:15 +0530

    Added: code to test null, type, ref , value check equality

--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -1,4 +1,46 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class QuantityMeasurementTest {
+    //FEET
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    //NULL CHECK
+    @Test
+    public void givenAnd0FeetNullFeet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    //REF CHECK
+    @Test
+    public void GivenReference0FeetAnd1Feet_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    //TYPE CHECK
+    @Test
+    public void GivenTypeCheckFor0FeetAnd1Feet_ShouldReturnEquals() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertEquals(feet1.getClass(), feet2.getClass());
+    }
+
 }
